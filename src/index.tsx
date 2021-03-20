3abd603117a7cacd9c0ad273ba16bad34b51ff32
aslansabyrov <48182963+aslansabyrov@users.noreply.github.com>
2021-03-21 05:02:30 +0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-
+import {  BrowserRouter} from "react-router-dom";
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
