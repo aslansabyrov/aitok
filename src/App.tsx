148652713cbe70c6d819d295845e716b1dd71f45
aslansabyrov <48182963+aslansabyrov@users.noreply.github.com>
2021-03-20 19:48:34 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -119,7 +119,7 @@ const App: React.FC = () => {
                   "Могут увидеть ваше мини-приложение и стать его пользователями"
                 }
                 buttonTitle={"Интересно"}
-                imgSrc={"/assets/video_text.png"}
+                imgSrc={"/assets/video_upd_text-removebg-preview.png"}
             ></SlideContent>
           </IonSlide>
           <IonSlide>
